7256db1c0fee9bcd839c8ddaec61056b67ec90dc
da-mckeel <damckeel13@gmail.com>
2026-01-31 14:40:50 -0600

    Added prompt to have multiple messages and verbose output flag for metadata

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,8 +10,17 @@ if api_key == None:
     raise RuntimeError("Missing Gemini API Key")
 
 client = genai.Client(api_key=api_key)
-prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt)
+
+# Prompting
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+# Response
+response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
 if response.usage_metadata == None:
     raise RuntimeError("Failed API Request")
 prompt_tokens = response.usage_metadata.prompt_token_count
@@ -17,11 +28,15 @@ response_tokens = response.usage_metadata.candidates_token_count
 
 
 def main():
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
-    print(f"Response: ")
-    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+        print(f"Response: ")
+        print(response.text)
+    else:
+        print(f"Response: ")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
