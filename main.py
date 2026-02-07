06bd2367ac3b3f828f360c9294ee1e19597735bd
da-mckeel <damckeel13@gmail.com>
2026-02-06 21:56:41 -0600

    Add system prompt and function declaration to generate content config

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,22 +22,28 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 # Response
-response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+response = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
+)
 if response.usage_metadata == None:
     raise RuntimeError("Failed API Request")
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
-
 def main():
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
-        print(f"Response: ")
-        print(response.text)
+    function_calls = response.function_calls
+
+    if function_calls is not None and len(function_calls) > 0:
+        for f in function_calls:
+            print(f"Calling function: {f.name}({f.args})")
     else:
-        print(f"Response: ")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+        print("Response: ")
         print(response.text)
 
 if __name__ == "__main__":
